93533ec3abf3bfb565559cd0b850b635e0a8752f
Jing Liang <jing.liang@centene.com>
2021-02-02 15:23:26 -0800

    update v2 version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello World - BlueGreen V1</h1>"
+    return "<h1>Hello World - BlueGreen V2</h1>"
 
 
 if __name__ == '__main__':
